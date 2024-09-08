30bf26f7a8954cd434946f7c3761c7d1924b0509
Saad Salman <saadsalmanakram1@gmail.com>
2024-09-09 02:41:41 +0500

    Create 19- Image Captioning Bot.py

---
+++ 1- Understanding Gradio/16- Chatbots/19- Image Captioning Bot.py
@@ -0,0 +1,16 @@
+import gradio as gr
+from PIL import Image
+import requests
+from transformers import BlipProcessor, BlipForConditionalGeneration
+
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+def generate_caption(image, history):
+    raw_image = Image.open(image).convert('RGB')
+    inputs = processor(raw_image, return_tensors="pt")
+    out = model.generate(**inputs)
+    caption = processor.decode(out[0], skip_special_tokens=True)
+    return caption
+
+gr.Interface(fn=generate_caption, inputs=gr.Image(), outputs="text").launch()
