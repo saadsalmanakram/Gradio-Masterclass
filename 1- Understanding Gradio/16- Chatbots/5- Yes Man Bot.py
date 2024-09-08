ea424e0b00b9db3f06cc0996cbc3f12a05d4b0fe
Saad Salman <saadsalmanakram1@gmail.com>
2024-09-09 02:28:01 +0500

    Create 5- Yes Man Bot.py

---
+++ 1- Understanding Gradio/16- Chatbots/5- Yes Man Bot.py
@@ -0,0 +1,21 @@
+import gradio as gr
+
+def yes_man(message, history):
+    if message.endswith("?"):
+        return "Yes"
+    else:
+        return "Ask me anything!"
+
+gr.ChatInterface(
+    yes_man,
+    chatbot=gr.Chatbot(height=300),
+    textbox=gr.Textbox(placeholder="Ask me a yes or no question", container=False, scale=7),
+    title="Yes Man",
+    description="Ask Yes Man any question",
+    theme="soft",
+    examples=["Hello", "Am I cool?", "Are tomatoes vegetables?"],
+    cache_examples=True,
+    retry_btn=None,
+    undo_btn="Delete Previous",
+    clear_btn="Clear",
+).launch()
