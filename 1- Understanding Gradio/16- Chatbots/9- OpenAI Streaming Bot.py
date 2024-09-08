077a6f3de885472f15f481fe8f8492f9cb3eb264
Saad Salman <saadsalmanakram1@gmail.com>
2024-09-09 02:34:43 +0500

    Create 9- OpenAI Streaming Bot.py

---
+++ 1- Understanding Gradio/16- Chatbots/9- OpenAI Streaming Bot.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+import gradio as gr
+
+api_key = "sk-..."  # Replace with your key
+client = OpenAI(api_key=api_key)
+
+def predict(message, history):
+    history_openai_format = []
+    for human, assistant in history:
+        history_openai_format.append({"role": "user", "content": human })
+        history_openai_format.append({"role": "assistant", "content":assistant})
+    history_openai_format.append({"role": "user", "content": message})
+  
+    response = client.chat.completions.create(model='gpt-3.5-turbo',
+    messages= history_openai_format,
+    temperature=1.0,
+    stream=True)
+
+    partial_message = ""
+    for chunk in response:
+        if chunk.choices[0].delta.content is not None:
+              partial_message = partial_message + chunk.choices[0].delta.content
+              yield partial_message
+
+gr.ChatInterface(predict).launch()
